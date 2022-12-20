b534725e604850ccd7a12860db28b77cb37faf83
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2022-12-20 13:47:44 +0700

    first

---
+++ video-list.js
@@ -0,0 +1,77 @@
+let allVideos = [
+  {
+    name: "Section Intro",
+    src: "1. Section Intro",
+    id: "vid_1",
+  },
+  {
+    name: "What Is Node.js and Why Use It",
+    src: "2. What Is Node.js and Why Use It",
+    id: "vid_2",
+  },
+  {
+    name: "Running Javascript Outside the Browser",
+    src: "3. Running Javascript Outside the Browser",
+    id: "vid_3",
+  },
+  {
+    name: "Using Modules 1 Core Modules",
+    src: "4. Using Modules 1 Core Modules",
+    id: "vid_4",
+  },
+  {
+    name: "Reading and Writing Files",
+    src: "5. Reading and Writing Files",
+    id: "vid_5",
+  },
+  {
+    name: "Blocking and Non-Blocking Asynchronous Nature of Node.js",
+    src: "6. Blocking and Non-Blocking Asynchronous Nature of Node.js",
+    id: "vid_6",
+  },
+  {
+    name: "Reading and Writing Files Asynchronously",
+    src: "7. Reading and Writing Files Asynchronously",
+    id: "vid_7",
+  },
+  {
+    name: "Creating a Simple Web Server",
+    src: "8. Creating a Simple Web Server",
+    id: "vid_8",
+  },
+  {
+    name: "Routing",
+    src: "9. Routing",
+    id: "vid_9",
+  },
+  {
+    name: "Building a (Very) Simple API",
+    src: "10. Building a (Very) Simple API",
+    id: "vid_10",
+  },
+  {
+    name: "HTML Templating Building the Templates",
+    src: "11. HTML Templating Building the Templates",
+    id: "vid_11",
+  },
+  {
+    name: "HTML Templating Filling the Templates",
+    src: "12. HTML Templating Filling the Templates",
+    id: "vid_12",
+  },
+  {
+    name: "Parsing Variables from URLs",
+    src: "13. Parsing Variables from URLs",
+    id: "vid_13",
+  },
+  {
+    name: "Using Modules 2 Our Own Modules",
+    src: "14. Using Modules 2 Our Own Modules",
+    id: "vid_14",
+  },
+  {
+    name: "Introduction to NPM and the package.json File",
+    src: "15. Introduction to NPM and the package.json File",
+    id: "vid_15",
+  },
+];
